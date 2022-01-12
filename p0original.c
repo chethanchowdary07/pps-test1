e06285c3409cd8cb5d6209b9ab9487ace054ce9d
chethanchowdary <97119882+chethanchowdary07@users.noreply.github.com>
2022-01-12 03:10:40 +0000

    code to add 2 no by usind 4 functions

--- p0original.c
+++ p0original.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-   printf("my name is chethan");
+   printf("my name is chethan\n");
   return 0;
 }
--- p10original.c
+++ p10original.c
@@ -1,6 +1,1 @@
-# include <stdio.h>
-int main()
-{
-  printf("my name is chethan");
-  return 0;
-}
+
